aa8e1033b5c4d2a167fa84d6f8f3e65dc5b1e218
abooBala <abubakar.bala@whogohost.com>
2020-12-28 18:24:15 +0100

    Pass activity from SecondActivity to MainActivity

--- app/src/main/java/com/google/codelabs/mdc/java/twoactivitiesalpha/SecondActivity.java
+++ app/src/main/java/com/google/codelabs/mdc/java/twoactivitiesalpha/SecondActivity.java
@@ -3,14 +3,22 @@ package com.google.codelabs.mdc.java.twoactivitiesalpha;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+	public static final String EXTRA_REPLY = "com.google.codelabs.mdc.java.twoactivitiesalpha.EXTRA_REPLY";
+
+	private EditText mReply;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_second);
+
+		mReply = findViewById(R.id.editText_second);
 
 		Intent intent = getIntent();
 		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
@@ -18,5 +26,15 @@ public class SecondActivity extends AppCompatActivity {
 		textView.setText(message);
 
 
+	}
+
+	public void returnReply(View view) {
+
+		String reply = mReply.getText().toString();
+
+		Intent replyIntent = new Intent();
+		replyIntent.putExtra(EXTRA_REPLY, reply);
+		setResult(RESULT_OK, replyIntent);
+		finish();
 	}
 }
